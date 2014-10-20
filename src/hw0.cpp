308715483a43e412802a078168857baeb118e9be
atan009 <atan009@ucr.edu>
2014-10-19 20:23:31 -0700

    added comments in source file

--- src/hw0.cpp
+++ src/hw0.cpp
@@ -20,55 +20,55 @@ using namespace boost;
 
 int main(int argc, char *argv[]) {
 	
-	bool T = true;
+	bool T = true;//continuous loop to run entire program
 	while (T == true) {
-		string cmds;
-		getline(cin, cmds);
+		string cmds;//what the user types in
+		getline(cin, cmds);//takes what user types
 
-		int sz = cmds.size();
+		int sz = cmds.size();//size of what user typed in
 
-		char *parse = new char[sz+1];
-		strcpy(parse, cmds.c_str());
+		char *parse = new char[sz+1];//c string array using size of cmds
+		strcpy(parse, cmds.c_str());//turn cmds into cstring in parse
 	
-		int comment = 0;
-		for (int i = 0; i < cmds.size(); i++)
+		for (int i = 0; i < cmds.size(); i++)//loop for comments
 		{
-			if (cmds.at(i) == '#')
+			if (cmds.at(i) == '#')//if user inputed a comment
 			{
-			parse[i] = '\0';
+			parse[i] = '\0';//change char in parse to null
 			break;
-			}
+			}//break b/c everything after null char will be ignored
 		}
 	
-		char** cstr = new char*[sz+1];
+		char** cstr = new char*[sz+1];//what parse will be parsed into
 
-		int counter = 0;
+		int counter = 0;//used to keep track of the end of cstr
 
 
-        	char *pch = strtok(parse, " "); 
-        	while (pch != NULL)
+        	char *pch = strtok(parse, " ");//parsing with space char
+        	while (pch != NULL)//until we hit a null character
         	{
-        		cstr[counter] = pch; 
-        		counter++; 
-        		pch = strtok(NULL, " "); 
+        		cstr[counter] = pch;//parsing into cstr
+        		counter++;//increment the counter
+        		pch = strtok(NULL, " ");//continue parsing
        		}
-	        cstr[counter] = NULL;
+	        cstr[counter] = NULL;//to avoid a seg fault when running
 
-		pid_t pid = fork();
+		pid_t pid = fork();//child
 		
 
-		if (pid == -1)
+		if (pid == -1)//if a child fails
 		{
 			cout << "child failed" << endl;
 			exit(1);
 		}
 
-		else if(pid == 0)
+		else if(pid == 0)//if parent
 		{
-			int run = 0;
-			while (run < argc) {
-				execvp(cstr[run],cstr);
-				run++;
+			wait(0);//wait until child finishes
+			int run = 0;//starting number of commands
+			while (run < argc) {//first to last command
+				execvp(cstr[run],cstr);//run the commands
+				run++;//increment run
 			}
 			exit(0);
 		}
